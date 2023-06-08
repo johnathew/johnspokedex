6eed124ba707770b6ed0ae202bbffb54bd45a91f
johnathew <jakornegay01@gmail.com>
2023-06-08 14:35:43 -0500

    added scroll to flavor text

--- src/PokemonDetails.tsx
+++ src/PokemonDetails.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { PokemonContext } from "./PokemonContext";
 import { PokeContext } from "./pokemonActionTypes";
 import { FaWeightHanging } from "react-icons/fa";
-import {TfiRuler} from "react-icons/tfi"
+import { TfiRuler } from "react-icons/tfi";
 
 import { useData } from "./hooks/useData";
 
@@ -11,19 +11,23 @@ const PokemonDetails = () => {
   const sample: any = useData(`${pokemonData?.pokemon.species?.url}`);
 
   return (
-    <div className="w-[270px] flex h-[87px] absolute z-100 bg-red-200 bg-opacity-5 rounded-md  items-center ml-[409px] overflow-y-auto mt-[265px]"> 
-    <div className="flex flex-col space-y-2 ">  
-      <div className="flex gap-x-2 justify-center items-center">
-      <TfiRuler className="text-yellow-300 text-lg" />
-      <p>{pokemonData.pokemon.height}"</p>
-      </div>
+    <div className="w-[270px] flex h-[87px] absolute z-100 p-2 bg-red-200 bg-opacity-5 rounded-md overflow-auto items-center ml-[409px] mt-[265px]">
+      <div className="flex flex-col space-y-2 border-r-2 w-1/5 pr-1">
+        <div className="flex gap-x-1 justify-center items-center">
+          <TfiRuler className="text-yellow-300 text-lg" />
+          <p className="text-[9px]">{pokemonData.pokemon.height}"</p>
+        </div>
 
-      <div className="flex justify-center items-center">
-        <FaWeightHanging className="text-yellow-300 text-xl ml-2" />{" "}
-        <p>{pokemonData.pokemon.weight} lbs.</p>
+        <div className="flex justify-center items-center">
+          <FaWeightHanging className="text-yellow-300" />{" "}
+          <p className="text-[8px]">{pokemonData.pokemon.weight} lbs.</p>
+        </div>
       </div>
-      </div>  
-      <p className="text-[7px] w-full leading-tight tracking-wider ">{sample?.flavor_text} </p>
+      <div className="w-full  h-auto  overflow-x-scroll ml-2 pt-3 ">
+        <p className="text-[8px] leading-tight tracking-wider">
+          {sample?.flavor_text}{" "}
+        </p>
+      </div>
     </div>
   );
 };
